1c26c0abbc7a9b2f0ca1f3c9f7eae6fe5a2ed102
tresor-del <tresoradzanto7@gmail.com>
2025-09-20 01:51:37 +0000

    New fonct.

--- javascript/progress_bar.js
+++ javascript/progress_bar.js
@@ -1,8 +1,21 @@
-window.addEventListener("scroll", () => {
-  const scrollTop = window.scrollY;
-  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-  const scrollPercent = (scrollTop / docHeight) * 100;
+// Sélectionne tous les liens du menu
+const navLinks = document.querySelectorAll('.nav ul li a');
 
-//   document.getElementById("progress-bar").style.width = scrollPercent + "%";
-  document.getElementById("progress-bar-mobile").style.width = scrollPercent + "%";
+// Observer les sections visibles
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const id = entry.target.getAttribute('id');
+    const link = document.querySelector(`.nav ul li a[href="#${id}"]`);
+
+    if (entry.isIntersecting) {
+      navLinks.forEach(l => l.classList.remove('active'));
+      link.classList.add('active');
+    }
+  });
+}, { threshold: 0.6 }); // 60% visible
+
+// Applique l’observer à toutes les sections
+const sections = document.querySelectorAll('.main'); 
+sections.forEach(sec => {
+  observer.observe(sec);
 });
